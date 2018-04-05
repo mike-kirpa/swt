94697033f25a018494dc719626fde866543db270
Михаил Кирпа <mikhail.kirpa@gamenet.ru>
2018-04-05 19:21:44 +0300

    урок 44-46

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,11 +3,21 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
+
+    private int id;
     private final String groupName;
     private final String headerName;
     private final String footerName;
 
+    public GroupData(int id, String groupName, String headerName, String footerName) {
+        this.id = id;
+        this.groupName = groupName;
+        this.headerName = headerName;
+        this.footerName = footerName;
+    }
+
     public GroupData(String groupName, String headerName, String footerName) {
+        this.id = 0;
         this.groupName = groupName;
         this.headerName = headerName;
         this.footerName = footerName;
@@ -21,15 +31,12 @@ public class GroupData {
         return headerName;
     }
 
-    public String getFooterName() {
-        return footerName;
-    }
+    public String getFooterName() { return footerName; }
 
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "groupName='" + groupName + '\'' +
-                '}';
+    public int getId() { return id; }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     @Override
@@ -37,12 +44,23 @@ public class GroupData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(groupName, groupData.groupName);
+        return Objects.equals(id, groupData.id) &&
+                Objects.equals(groupName, groupData.groupName);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(groupName);
+        return Objects.hash(id, groupName);
     }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id='" + id + '\'' +
+                ", groupName='" + groupName + '\'' +
+                '}';
+    }
+
+
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase{
@@ -15,9 +16,17 @@ public class GroupCreationTests extends TestBase{
 
         app.getNavigationHelper().goToGroups();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        GroupData group = new GroupData("test1", null, null);
+        app.getGroupHelper().createGroup(group);
         app.getNavigationHelper().goToGroups();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+
+//список переводим в поток, далее по этому потоку проходит функция и находит максимальный элемент
+// (сраниваются объекты GroupData путем сравнения их идентификаторов Id), на выходе объект с максимальным идентификатором
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
