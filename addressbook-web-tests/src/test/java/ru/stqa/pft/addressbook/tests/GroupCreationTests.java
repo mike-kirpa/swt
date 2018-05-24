53df3a44b2a1262387217bb27fa48ebccec4f844
Михаил Кирпа <mikhail.kirpa@gamenet.ru>
2018-05-24 19:18:00 +0300

    Урок 62. Параметризация тестовых методов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,22 +1,35 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase{
 
+    //создаем фабрику данных - заполняем список массивов, возвращаем итератор на первый элемент списка.
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new GroupData().withGroupName("test1").withHeaderName("header 1").withFooterName("footer 1")});
+        list.add(new Object[] {new GroupData().withGroupName("test2").withHeaderName("header 2").withFooterName("footer 2")});
+        list.add(new Object[] {new GroupData().withGroupName("test3").withHeaderName("header 3").withFooterName("footer 3")});
+        return list.iterator();
+    }
 
-    @Test
-    public void testGroupCreation(){
-
+    //из фабрики массивов забираем элементы списка по итератору, передаем объкет GroupData
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group){
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withGroupName("test1");
         app.group().create(group);
         app.goTo().groupPage();
         assertThat(app.group().count(), equalTo(before.size() + 1));
